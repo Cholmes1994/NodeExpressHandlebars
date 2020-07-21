7c5c7d14cde56308a6bdfea380752f983d21a793
Christopher Holmes <holmes.chris94@yahoo.com>
2020-07-20 20:03:12 -0400

    Folder structure started, created burger database and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Black & Blue');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('BBQ Bacon Burger', true);
